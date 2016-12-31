ff14a4a92a88da78c2816267ab37508b2857adaf
fahadrao <fahadrao@gmail.com>
2016-12-31 21:17:29 +0500

    fifth commit, AD blog

--- aasandocblog/settings.py
+++ aasandocblog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'o_0bi_m!@omrhyb^y&y69qn!p6tj1m7&oqamv7^-h=rpoya@nl'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['laurenbenoit.pythonanywhere.com','127.0.0.1']
 
 
 # Application definition
--- blog/models.py
+++ blog/models.py
@@ -5,12 +5,12 @@ from django.utils import timezone
 # Create your models here.
 
 class Post(models.Model):
-    author = models.ForeignKey('auth.User')
-    title = models.CharField(max_length=200)
-    text = models.TextField()
-    created_date = models.DateTimeField(
+    name = models.ForeignKey('auth.User')
+    specialization = models.CharField(max_length=200)
+    history = models.TextField()
+    added_date = models.DateTimeField(
             default=timezone.now)
-    published_date = models.DateTimeField(
+    verified_date = models.DateTimeField(
             blank=True, null=True)
 
     def publish(self):
